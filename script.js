1a10f7ef3a79a732cf51849ca3a85d16c84070e4
tconnerii <connerdatasolutions@gmail.com>
2026-01-26 19:22:03 -0600

    Enhance button interaction with dynamic styles

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("JavaScript is connected!");
+const button = document.getElementById("alertBtn");
+const message = document.getElementById("message");
+
+let clicked = false;
+
+button.addEventListener("click", () => {
+    if (!clicked) {
+      message.textContent = "Button clicked 😎🫡";
+      message.style.color = "darkred";
+      message.style.fontSize = "24px";
+      clicked = true;  
+    } else {
+        message.textContent = "Waiting for interaction...";
+        message.style.color = "black";
+        message.style.fontSize = '16px';
+        clicked = false;
+    }
+});
